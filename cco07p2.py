17f75e280bd8f7ec3a0edda5a34fa70e2ced6532
BinAccel <BinAccel@users.noreply.github.com>
2016-12-12 12:16:22 -0500

    One liner Snowflakes
    Must be a meme.

--- cco07p2.py
+++ cco07p2.py
@@ -1,5 +1,1 @@
-from sys import stdin
-input = stdin.readline
-N = int(input())
-S = {tuple(sorted(input().split())) for _ in xrange(N)}
-print 'No two snowflakes are alike.' if len(S) == N else 'Twin snowflakes found.'
+print((lambda N: 'No two snowflakes are alike.' if len({tuple(sorted(raw_input().split())) for _ in xrange(N)}) == N else 'Twin snowflakes found.')(int(raw_input())))
